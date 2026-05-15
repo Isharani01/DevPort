6d52826dad1cd1b039e08416716a6bfb6279ed58
Isharani01 <ishakhizar2709@gmail.com>
2026-05-15 22:50:39 +0500

    updated profile.cs

--- DevPort/Models/Profile.cs
+++ DevPort/Models/Profile.cs
@@ -1,2 +1,17 @@
-﻿
+﻿namespace DevPort.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string About { get; set; } = string.Empty;
+
+        public string Phone { get; set; } = string.Empty;
+    }
+}
+
 
