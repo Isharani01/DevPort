68def2c0e36a14ff938b51924fdd4b6f60130148
247291-cmd <247291@students.au.edu.pk>
2026-05-17 13:18:59 +0500

    Fixed layout and program configuration errors

--- DevPort/Program.cs
+++ DevPort/Program.cs
@@ -37,10 +37,6 @@ var connectionString =
     builder.Configuration.GetConnectionString("DefaultConnection")
     ?? throw new InvalidOperationException(
         "Connection string not found.");
-var connectionString =
-    builder.Configuration.GetConnectionString("DefaultConnection")
-    ?? throw new InvalidOperationException(
-        "Connection string 'DefaultConnection' not found.");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
